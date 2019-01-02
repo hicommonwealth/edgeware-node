b301276fdf4300081f26e27d2f73e60945caf897
Drew Stone <drewstone329@gmail.com>
2019-01-03 00:36:30 +0200

    Formatting with rustfmt

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -3,10 +3,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(not(feature = "std"), feature(alloc))]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
-#![recursion_limit="256"]
+#![recursion_limit = "256"]
 
-extern crate sr_std as rstd;
 extern crate sr_io as runtime_io;
+extern crate sr_std as rstd;
 #[macro_use]
 extern crate substrate_client as client;
 #[macro_use]
@@ -16,53 +16,54 @@ extern crate sr_primitives as runtime_primitives;
 #[cfg(feature = "std")]
 #[macro_use]
 extern crate serde_derive;
-extern crate substrate_primitives as primitives;
 extern crate parity_codec;
+extern crate substrate_primitives as primitives;
 #[macro_use]
 extern crate parity_codec_derive;
 #[macro_use]
 extern crate sr_version as version;
-extern crate srml_system as system;
-extern crate srml_executive as executive;
-extern crate srml_consensus as consensus;
-extern crate srml_timestamp as timestamp;
-extern crate srml_balances as balances;
-extern crate srml_upgrade_key as upgrade_key;
-extern crate srml_aura as aura;
-extern crate srml_session as session;
-extern crate substrate_consensus_aura_primitives as consensus_aura;
-extern crate edge_identity;
 extern crate edge_delegation;
 extern crate edge_governance;
+extern crate edge_identity;
+extern crate srml_aura as aura;
+extern crate srml_balances as balances;
+extern crate srml_consensus as consensus;
+extern crate srml_executive as executive;
+extern crate srml_session as session;
+extern crate srml_system as system;
+extern crate srml_timestamp as timestamp;
+extern crate srml_upgrade_key as upgrade_key;
+extern crate substrate_consensus_aura_primitives as consensus_aura;
 
-use edge_identity::identity;
 use edge_delegation::delegation;
 use edge_governance::governance;
+use edge_identity::identity;
 
-use rstd::prelude::*;
+use client::{block_builder::api as block_builder_api, runtime_api};
+use consensus_aura::api as aura_api;
 #[cfg(feature = "std")]
 use primitives::bytes;
 use primitives::{AuthorityId, OpaqueMetadata};
+use rstd::prelude::*;
 use runtime_primitives::{
-	ApplyResult, transaction_validity::TransactionValidity, Ed25519Signature, generic,
-	traits::{self, BlakeTwo256, Block as BlockT, ProvideInherent, Convert},
-	BasicInherentData, CheckInherentError
+    generic,
+    traits::{self, BlakeTwo256, Block as BlockT, Convert, ProvideInherent},
+    transaction_validity::TransactionValidity,
+    ApplyResult, BasicInherentData, CheckInherentError, Ed25519Signature,
 };
-use client::{block_builder::api as block_builder_api, runtime_api};
-use version::RuntimeVersion;
 #[cfg(feature = "std")]
 use version::NativeVersion;
-use consensus_aura::api as aura_api;
+use version::RuntimeVersion;
 
 // A few exports that help ease life for downstream crates.
+pub use balances::Call as BalancesCall;
+pub use consensus::Call as ConsensusCall;
 #[cfg(any(feature = "std", test))]
 pub use runtime_primitives::BuildStorage;
-pub use consensus::Call as ConsensusCall;
-pub use timestamp::Call as TimestampCall;
-pub use balances::Call as BalancesCall;
-pub use runtime_primitives::{Permill, Perbill};
+pub use runtime_primitives::{Perbill, Permill};
+pub use srml_support::{RuntimeMetadata, StorageValue};
 pub use timestamp::BlockPeriod;
-pub use srml_support::{StorageValue, RuntimeMetadata};
+pub use timestamp::Call as TimestampCall;
 
 /// Alias to Ed25519 pubkey that identifies an account on the chain.
 pub type AccountId = primitives::H256;
@@ -83,91 +84,92 @@ pub type SessionKey = AuthorityId;
 /// of data like extrinsics, allowing for them to continue syncing the network through upgrades
 /// to even the core datastructures.
 pub mod opaque {
-	use super::*;
+    use super::*;
 
-	/// Opaque, encoded, unchecked extrinsic.
-	#[derive(PartialEq, Eq, Clone, Default, Encode, Decode)]
-	#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
-	pub struct UncheckedExtrinsic(#[cfg_attr(feature = "std", serde(with="bytes"))] pub Vec<u8>);
-	impl traits::Extrinsic for UncheckedExtrinsic {
-		fn is_signed(&self) -> Option<bool> {
-			None
-		}
-	}
-	/// Opaque block header type.
-	pub type Header = generic::Header<BlockNumber, BlakeTwo256, generic::DigestItem<Hash, AuthorityId>>;
-	/// Opaque block type.
-	pub type Block = generic::Block<Header, UncheckedExtrinsic>;
-	/// Opaque block identifier type.
-	pub type BlockId = generic::BlockId<Block>;
-	/// Opaque session key type.
-	pub type SessionKey = AuthorityId;
+    /// Opaque, encoded, unchecked extrinsic.
+    #[derive(PartialEq, Eq, Clone, Default, Encode, Decode)]
+    #[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+    pub struct UncheckedExtrinsic(#[cfg_attr(feature = "std", serde(with = "bytes"))] pub Vec<u8>);
+    impl traits::Extrinsic for UncheckedExtrinsic {
+        fn is_signed(&self) -> Option<bool> {
+            None
+        }
+    }
+    /// Opaque block header type.
+    pub type Header =
+        generic::Header<BlockNumber, BlakeTwo256, generic::DigestItem<Hash, AuthorityId>>;
+    /// Opaque block type.
+    pub type Block = generic::Block<Header, UncheckedExtrinsic>;
+    /// Opaque block identifier type.
+    pub type BlockId = generic::BlockId<Block>;
+    /// Opaque session key type.
+    pub type SessionKey = AuthorityId;
 }
 
 /// This runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("edgeware"),
-	impl_name: create_runtime_str!("edgeware"),
-	authoring_version: 1,
-	spec_version: 1,
-	impl_version: 0,
-	apis: RUNTIME_API_VERSIONS,
+    spec_name: create_runtime_str!("edgeware"),
+    impl_name: create_runtime_str!("edgeware"),
+    authoring_version: 1,
+    spec_version: 1,
+    impl_version: 0,
+    apis: RUNTIME_API_VERSIONS,
 };
 
 /// The version infromation used to identify this runtime when compiled natively.
 #[cfg(feature = "std")]
 pub fn native_version() -> NativeVersion {
-	NativeVersion {
-		runtime_version: VERSION,
-		can_author_with: Default::default(),
-	}
+    NativeVersion {
+        runtime_version: VERSION,
+        can_author_with: Default::default(),
+    }
 }
 
 impl system::Trait for Runtime {
-	/// The identifier used to distinguish between accounts.
-	type AccountId = AccountId;
-	/// The index type for storing how many extrinsics an account has signed.
-	type Index = Nonce;
-	/// The index type for blocks.
-	type BlockNumber = BlockNumber;
-	/// The type for hashing blocks and tries.
-	type Hash = Hash;
-	/// The hashing algorithm used.
-	type Hashing = BlakeTwo256;
-	/// The header digest type.
-	type Digest = generic::Digest<Log>;
-	/// The header type.
-	type Header = generic::Header<BlockNumber, BlakeTwo256, Log>;
-	/// The ubiquitous event type.
-	type Event = Event;
-	/// The ubiquitous log type.
-	type Log = Log;
-	/// The ubiquitous origin type.
-	type Origin = Origin;
+    /// The identifier used to distinguish between accounts.
+    type AccountId = AccountId;
+    /// The index type for storing how many extrinsics an account has signed.
+    type Index = Nonce;
+    /// The index type for blocks.
+    type BlockNumber = BlockNumber;
+    /// The type for hashing blocks and tries.
+    type Hash = Hash;
+    /// The hashing algorithm used.
+    type Hashing = BlakeTwo256;
+    /// The header digest type.
+    type Digest = generic::Digest<Log>;
+    /// The header type.
+    type Header = generic::Header<BlockNumber, BlakeTwo256, Log>;
+    /// The ubiquitous event type.
+    type Event = Event;
+    /// The ubiquitous log type.
+    type Log = Log;
+    /// The ubiquitous origin type.
+    type Origin = Origin;
 }
 
 impl aura::Trait for Runtime {
-	type HandleReport = ();
+    type HandleReport = ();
 }
 
 impl consensus::Trait for Runtime {
-	/// The position in the block's extrinsics that the note-offline inherent must be placed.
-	const NOTE_OFFLINE_POSITION: u32 = 1;
-	/// The identifier we use to refer to authorities.
-	type SessionKey = AuthorityId;
-	// The aura module handles offline-reports internally
-	// rather than using an explicit report system.
-	type InherentOfflineReport = ();
-	/// The ubiquitous log type.
-	type Log = Log;
+    /// The position in the block's extrinsics that the note-offline inherent must be placed.
+    const NOTE_OFFLINE_POSITION: u32 = 1;
+    /// The identifier we use to refer to authorities.
+    type SessionKey = AuthorityId;
+    // The aura module handles offline-reports internally
+    // rather than using an explicit report system.
+    type InherentOfflineReport = ();
+    /// The ubiquitous log type.
+    type Log = Log;
 }
 
 impl timestamp::Trait for Runtime {
-	/// The position in the block's extrinsics that the timestamp-set inherent must be placed.
-	const TIMESTAMP_SET_POSITION: u32 = 0;
-	/// A timestamp: seconds since the unix epoch.
-	type Moment = u64;
-	type OnTimestampSet = Aura;
+    /// The position in the block's extrinsics that the timestamp-set inherent must be placed.
+    const TIMESTAMP_SET_POSITION: u32 = 0;
+    /// A timestamp: seconds since the unix epoch.
+    type Moment = u64;
+    type OnTimestampSet = Aura;
 }
 
 /// Session key conversion.
@@ -185,38 +187,38 @@ impl session::Trait for Runtime {
 }
 
 impl balances::Trait for Runtime {
-	/// The type for recording an account's balance.
-	type Balance = u128;
-	/// The type for recording indexing into the account enumeration. If this ever overflows, there
-	/// will be problems!
-	type AccountIndex = u32;
-	/// What to do if an account's free balance gets zeroed.
-	type OnFreeBalanceZero = ();
-	/// Restrict whether an account can transfer funds. We don't place any further restrictions.
-	type EnsureAccountLiquid = ();
-	/// The uniquitous event type.
-	type Event = Event;
+    /// The type for recording an account's balance.
+    type Balance = u128;
+    /// The type for recording indexing into the account enumeration. If this ever overflows, there
+    /// will be problems!
+    type AccountIndex = u32;
+    /// What to do if an account's free balance gets zeroed.
+    type OnFreeBalanceZero = ();
+    /// Restrict whether an account can transfer funds. We don't place any further restrictions.
+    type EnsureAccountLiquid = ();
+    /// The uniquitous event type.
+    type Event = Event;
 }
 
 impl upgrade_key::Trait for Runtime {
-	/// The uniquitous event type.
-	type Event = Event;
+    /// The uniquitous event type.
+    type Event = Event;
 }
 impl identity::Trait for Runtime {
-	/// The type for making a claim to an identity.
-	type Claim = Vec<u8>;
-	/// The uniquitous event type.
-	type Event = Event;
+    /// The type for making a claim to an identity.
+    type Claim = Vec<u8>;
+    /// The uniquitous event type.
+    type Event = Event;
 }
 
 impl delegation::Trait for Runtime {
-	/// The uniquitous event type.
-  type Event = Event;
+    /// The uniquitous event type.
+    type Event = Event;
 }
 
 impl governance::Trait for Runtime {
-	/// The uniquitous event type.
-  type Event = Event;
+    /// The uniquitous event type.
+    type Event = Event;
 }
 
 construct_runtime!(
@@ -230,11 +232,11 @@ construct_runtime!(
 		Consensus: consensus::{Module, Call, Storage, Config<T>, Log(AuthoritiesChange), Inherent},
 		Aura: aura::{Module},
 		Balances: balances,
-    Session: session,
+		Session: session,
 		UpgradeKey: upgrade_key,
-  	Identity: identity::{Module, Call, Storage, Config<T>, Event<T>},
-    Delegation: delegation::{Module, Call, Storage, Event<T>},
-    Governance: governance::{Module, Call, Storage, Event<T>},
+		Identity: identity::{Module, Call, Storage, Config<T>, Event<T>},
+		Delegation: delegation::{Module, Call, Storage, Event<T>},
+		Governance: governance::{Module, Call, Storage, Event<T>},
 	}
 );
 
@@ -249,7 +251,8 @@ pub type Block = generic::Block<Header, UncheckedExtrinsic>;
 /// BlockId type as expected by this runtime.
 pub type BlockId = generic::BlockId<Block>;
 /// Unchecked extrinsic type as expected by this runtime.
-pub type UncheckedExtrinsic = generic::UncheckedMortalExtrinsic<Address, Nonce, Call, Ed25519Signature>;
+pub type UncheckedExtrinsic =
+    generic::UncheckedMortalExtrinsic<Address, Nonce, Call, Ed25519Signature>;
 /// Extrinsic type that has already been checked.
 pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, Nonce, Call>;
 /// Executive: handles dispatch to the various modules.
@@ -257,76 +260,76 @@ pub type Executive = executive::Executive<Runtime, Block, Context, Balances, All
 
 // Implement our runtime API endpoints. This is just a bunch of proxying.
 impl_runtime_apis! {
-	impl runtime_api::Core<Block> for Runtime {
-		fn version() -> RuntimeVersion {
-			VERSION
-		}
+    impl runtime_api::Core<Block> for Runtime {
+        fn version() -> RuntimeVersion {
+            VERSION
+        }
 
-		fn authorities() -> Vec<AuthorityId> {
-			Consensus::authorities()
-		}
+        fn authorities() -> Vec<AuthorityId> {
+            Consensus::authorities()
+        }
 
-		fn execute_block(block: Block) {
-			Executive::execute_block(block)
-		}
+        fn execute_block(block: Block) {
+            Executive::execute_block(block)
+        }
 
-		fn initialise_block(header: <Block as BlockT>::Header) {
-			Executive::initialise_block(&header)
-		}
-	}
+        fn initialise_block(header: <Block as BlockT>::Header) {
+            Executive::initialise_block(&header)
+        }
+    }
 
-	impl runtime_api::Metadata<Block> for Runtime {
-		fn metadata() -> OpaqueMetadata {
-			Runtime::metadata().into()
-		}
-	}
+    impl runtime_api::Metadata<Block> for Runtime {
+        fn metadata() -> OpaqueMetadata {
+            Runtime::metadata().into()
+        }
+    }
 
-	impl block_builder_api::BlockBuilder<Block, BasicInherentData> for Runtime {
-		fn apply_extrinsic(extrinsic: <Block as BlockT>::Extrinsic) -> ApplyResult {
-			Executive::apply_extrinsic(extrinsic)
-		}
+    impl block_builder_api::BlockBuilder<Block, BasicInherentData> for Runtime {
+        fn apply_extrinsic(extrinsic: <Block as BlockT>::Extrinsic) -> ApplyResult {
+            Executive::apply_extrinsic(extrinsic)
+        }
 
-		fn finalise_block() -> <Block as BlockT>::Header {
-			Executive::finalise_block()
-		}
+        fn finalise_block() -> <Block as BlockT>::Header {
+            Executive::finalise_block()
+        }
 
-		fn inherent_extrinsics(data: BasicInherentData) -> Vec<<Block as BlockT>::Extrinsic> {
-			let mut inherent = Vec::new();
+        fn inherent_extrinsics(data: BasicInherentData) -> Vec<<Block as BlockT>::Extrinsic> {
+            let mut inherent = Vec::new();
 
-			inherent.extend(
-				Timestamp::create_inherent_extrinsics(data.timestamp)
-					.into_iter()
-					.map(|v| (v.0, UncheckedExtrinsic::new_unsigned(Call::Timestamp(v.1))))
-			);
+            inherent.extend(
+                Timestamp::create_inherent_extrinsics(data.timestamp)
+                    .into_iter()
+                    .map(|v| (v.0, UncheckedExtrinsic::new_unsigned(Call::Timestamp(v.1))))
+            );
 
-			inherent.extend(
-				Consensus::create_inherent_extrinsics(data.consensus)
-					.into_iter()
-					.map(|v| (v.0, UncheckedExtrinsic::new_unsigned(Call::Consensus(v.1))))
-			);
+            inherent.extend(
+                Consensus::create_inherent_extrinsics(data.consensus)
+                    .into_iter()
+                    .map(|v| (v.0, UncheckedExtrinsic::new_unsigned(Call::Consensus(v.1))))
+            );
 
-			inherent.as_mut_slice().sort_unstable_by_key(|v| v.0);
-			inherent.into_iter().map(|v| v.1).collect()
-		}
+            inherent.as_mut_slice().sort_unstable_by_key(|v| v.0);
+            inherent.into_iter().map(|v| v.1).collect()
+        }
 
-		fn check_inherents(block: Block, data: BasicInherentData) -> Result<(), CheckInherentError> {
-			Runtime::check_inherents(block, data)
-		}
+        fn check_inherents(block: Block, data: BasicInherentData) -> Result<(), CheckInherentError> {
+            Runtime::check_inherents(block, data)
+        }
 
-		fn random_seed() -> <Block as BlockT>::Hash {
-			System::random_seed()
-		}
-	}
+        fn random_seed() -> <Block as BlockT>::Hash {
+            System::random_seed()
+        }
+    }
 
-	impl runtime_api::TaggedTransactionQueue<Block> for Runtime {
-		fn validate_transaction(tx: <Block as BlockT>::Extrinsic) -> TransactionValidity {
-			Executive::validate_transaction(tx)
-		}
-	}
+    impl runtime_api::TaggedTransactionQueue<Block> for Runtime {
+        fn validate_transaction(tx: <Block as BlockT>::Extrinsic) -> TransactionValidity {
+            Executive::validate_transaction(tx)
+        }
+    }
 
-	impl aura_api::AuraApi<Block> for Runtime {
-		fn slot_duration() -> u64 {
-			Aura::slot_duration()
-		}
-	}
+    impl aura_api::AuraApi<Block> for Runtime {
+        fn slot_duration() -> u64 {
+            Aura::slot_duration()
+        }
+    }
 }
--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -1,14 +1,8 @@
-use primitives::{AuthorityId, ed25519};
 use edgeware_runtime::{
-	AccountId,
-	GenesisConfig,
-	ConsensusConfig,
-  SessionConfig,
-	TimestampConfig,
-	BalancesConfig,
-	UpgradeKeyConfig,
-	IdentityConfig,
+    AccountId, BalancesConfig, ConsensusConfig, GenesisConfig, IdentityConfig, SessionConfig,
+    TimestampConfig, UpgradeKeyConfig,
 };
+use primitives::{ed25519, AuthorityId};
 use substrate_service;
 
 // Note this is the URL for the telemetry server
@@ -22,77 +16,122 @@ pub type ChainSpec = substrate_service::ChainSpec<GenesisConfig>;
 /// from a string (`--chain=...`) into a `ChainSpec`.
 #[derive(Clone, Debug)]
 pub enum Alternative {
-	/// Whatever the current runtime is, with just Alice as an auth.
-	Development,
-	/// Whatever the current runtime is, with simple Alice/Bob auths.
-	LocalTestnet,
+    /// Whatever the current runtime is, with just Alice as an auth.
+    Development,
+    /// Whatever the current runtime is, with simple Alice/Bob auths.
+    LocalTestnet,
     /// Whatever the current runtime is, with all lock droppers and valid authorities
     Edgeware,
 }
 
 impl Alternative {
-	/// Get an actual chain config from one of the alternatives.
-	pub(crate) fn load(self) -> Result<ChainSpec, String> {
-		Ok(match self {
-			Alternative::Development => ChainSpec::from_genesis(
-				"Development",
-				"development",
-				|| testnet_genesis(vec![
-					ed25519::Pair::from_seed(b"Alice                           ").public().into(),
-				], vec![
-					ed25519::Pair::from_seed(b"Alice                           ").public().0.into(),
-				],
-					ed25519::Pair::from_seed(b"Alice                           ").public().0.into()
-				),
-				vec![],
-				None,
-				None,
-				None,
-        None
-			),
-			Alternative::LocalTestnet => ChainSpec::from_genesis(
-				"Local Testnet",
-				"local_testnet",
-				|| testnet_genesis(vec![
-					ed25519::Pair::from_seed(b"Alice                           ").public().into(),
-					ed25519::Pair::from_seed(b"Bob                             ").public().into(),
-				], vec![
-					ed25519::Pair::from_seed(b"Alice                           ").public().0.into(),
-					ed25519::Pair::from_seed(b"Bob                             ").public().0.into(),
-					ed25519::Pair::from_seed(b"Charlie                         ").public().0.into(),
-					ed25519::Pair::from_seed(b"Dave                            ").public().0.into(),
-					ed25519::Pair::from_seed(b"Eve                             ").public().0.into(),
-					ed25519::Pair::from_seed(b"Ferdie                          ").public().0.into(),
-				],
-					ed25519::Pair::from_seed(b"Alice                           ").public().0.into()
-				),
-				vec![],
-				None,
-				None,
-				None,
-        None
-			),
+    /// Get an actual chain config from one of the alternatives.
+    pub(crate) fn load(self) -> Result<ChainSpec, String> {
+        Ok(match self {
+            Alternative::Development => ChainSpec::from_genesis(
+                "Development",
+                "development",
+                || {
+                    testnet_genesis(
+                        vec![
+                            ed25519::Pair::from_seed(b"Alice                           ")
+                                .public()
+                                .into(),
+                        ],
+                        vec![
+                            ed25519::Pair::from_seed(b"Alice                           ")
+                                .public()
+                                .0
+                                .into(),
+                        ],
+                        ed25519::Pair::from_seed(b"Alice                           ")
+                            .public()
+                            .0
+                            .into(),
+                    )
+                },
+                vec![],
+                None,
+                None,
+                None,
+                None,
+            ),
+            Alternative::LocalTestnet => ChainSpec::from_genesis(
+                "Local Testnet",
+                "local_testnet",
+                || {
+                    testnet_genesis(
+                        vec![
+                            ed25519::Pair::from_seed(b"Alice                           ")
+                                .public()
+                                .into(),
+                            ed25519::Pair::from_seed(b"Bob                             ")
+                                .public()
+                                .into(),
+                        ],
+                        vec![
+                            ed25519::Pair::from_seed(b"Alice                           ")
+                                .public()
+                                .0
+                                .into(),
+                            ed25519::Pair::from_seed(b"Bob                             ")
+                                .public()
+                                .0
+                                .into(),
+                            ed25519::Pair::from_seed(b"Charlie                         ")
+                                .public()
+                                .0
+                                .into(),
+                            ed25519::Pair::from_seed(b"Dave                            ")
+                                .public()
+                                .0
+                                .into(),
+                            ed25519::Pair::from_seed(b"Eve                             ")
+                                .public()
+                                .0
+                                .into(),
+                            ed25519::Pair::from_seed(b"Ferdie                          ")
+                                .public()
+                                .0
+                                .into(),
+                        ],
+                        ed25519::Pair::from_seed(b"Alice                           ")
+                            .public()
+                            .0
+                            .into(),
+                    )
+                },
+                vec![],
+                None,
+                None,
+                None,
+                None,
+            ),
             Alternative::Edgeware => {
                 match ChainSpec::from_json_file(std::path::PathBuf::from("edgeware_testnet.json")) {
                     Ok(spec) => spec,
-                    Err(_) => panic!()
+                    Err(_) => panic!(),
                 }
             }
-        }) 
-	}
+        })
+    }
 
-	pub(crate) fn from(s: &str) -> Option<Self> {
-		match s {
-			"dev" => Some(Alternative::Development),
-			"local" => Some(Alternative::LocalTestnet),
+    pub(crate) fn from(s: &str) -> Option<Self> {
+        match s {
+            "dev" => Some(Alternative::Development),
+            "local" => Some(Alternative::LocalTestnet),
             "edgeware" => Some(Alternative::Edgeware),
-			_ => None,
-		}
-	}
+            _ => None,
+        }
+    }
 }
 
-fn testnet_genesis(initial_authorities: Vec<AuthorityId>, endowed_accounts: Vec<AccountId>, upgrade_key: AccountId) -> GenesisConfig {
-	GenesisConfig {
+fn testnet_genesis(
+    initial_authorities: Vec<AuthorityId>,
+    endowed_accounts: Vec<AccountId>,
+    upgrade_key: AccountId,
+) -> GenesisConfig {
+    GenesisConfig {
 		consensus: Some(ConsensusConfig {
 			code: include_bytes!("../runtime/wasm/target/wasm32-unknown-unknown/release/edgeware_runtime.compact.wasm").to_vec(),
 			authorities: initial_authorities.clone(),
