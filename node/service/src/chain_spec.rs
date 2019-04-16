b2aa13c42483293d9f04d07c18f4fa88488c1db2
Drew Stone <drewstone329@gmail.com>
2019-04-16 23:20:42 +0300

    New testnet

--- node/service/src/chain_spec.rs
+++ node/service/src/chain_spec.rs
@@ -32,7 +32,7 @@ const DEFAULT_PROTOCOL_ID: &str = "edg";
 pub type ChainSpec = substrate_service::ChainSpec<GenesisConfig>;
 
 pub fn edgeware_config() -> ChainSpec {
-	match ChainSpec::from_json_file(std::path::PathBuf::from("testnets/v0.1.9/edgeware.json")) {
+	match ChainSpec::from_json_file(std::path::PathBuf::from("testnets/v0.2.0/edgeware.json")) {
 		Ok(spec) => spec,
 		Err(e) => panic!(e),
 	}
