bca64089e912155f819f829a01d4220ee7895518
Drew Stone <drewstone329@gmail.com>
2019-05-02 17:53:27 +0300

    Fix keygen

--- keygen.sh
+++ keygen.sh
@@ -1,5 +1,5 @@
-default=$(subkey generate | grep -o '`.*`' | tr -d '`')
-mnemonic=${1:-$default}
+new_mnemonic=$(subkey generate | grep -o '`.*`' | tr -d '`')
+mnemonic=${1:-$new_mnemonic}
 
 stash_seed=$(subkey -e inspect "${mnemonic}"//stash | grep -o ': .*' | sed '1!d' | tr -d ': ')
 stash_pubkey=$(subkey -e inspect "${mnemonic}"//stash | grep -o ': .*' | sed '2!d' | tr -d ': ')
@@ -13,14 +13,26 @@ authority_seed=$(subkey -e inspect "${mnemonic}"//authority | grep -o ': .*' | s
 authority_pubkey=$(subkey -e inspect "${mnemonic}"//authority | grep -o ': .*' | sed '2!d' | tr -d ': ')
 authority_address=$(subkey -e inspect "${mnemonic}"//authority | grep -o ': .*' | sed '3!d' | tr -d ': ')
 
+echo ""
+echo "*********** SAVE THIS MNEMONIC FOR FUTURE USE OR RISK LOSING ACCESS TO ANY FUNDS ***********"
+echo ""
 echo "Mnemonic: ${mnemonic}"
+echo ""
+echo "********************************************************************************************"
+echo ""
+echo "STASH ACCOUNT FOR STORING FUNDS TO DELEGATE TO VALIDATORS OR GENERAL USE"
+echo ""
 echo "Stash seed: ${stash_seed}"
 echo "Stash pubkey: ${stash_pubkey}"
 echo "Stash address: ${stash_address}"
 echo ""
+echo "*********** CONTROLLER ACCOUNT FOR CONTROLLING A VALIDATOR NODE OR GENERAL USE ***********"
+echo ""
 echo "Controller seed: ${controller_seed}"
 echo "Controller pubkey: ${controller_pubkey}"
 echo "Controller address: ${controller_address}"
+echo ""
+echo "*********** AUTHORITY ACCOUNT FOR CONTROLLING A GRANDPA AUTHORITY/VALIDATOR NODE OR GENERAL USE ***********"
 echo ""
 echo "Authority seed: ${authority_seed}"
 echo "Authority pubkey: ${authority_pubkey}"
