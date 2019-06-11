bcaeaf30c0839bb68ff0860aa4ce0ff705c367ce
Drew Stone <drewstone329@gmail.com>
2019-06-11 21:09:56 +0300

    Ensure rust updates happen regardless

--- setup.sh
+++ setup.sh
@@ -2,33 +2,34 @@
 
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	echo "Found linux"
-  # check for root
-  SUDO_PREFIX=''
-  if [[ $EUID -ne 0 ]]; then
-      echo "Running apt as sudo"
-      SUDO_PREFIX='sudo'
-  fi
-	$SUDO_PREFIX apt install -y build-essential cmake pkg-config libssl-dev openssl git clang libclang-dev
+    echo "Found linux"
+    # check for root
+    SUDO_PREFIX=''
+    if [[ $EUID -ne 0 ]]; then
+        echo "Running apt as sudo"
+        SUDO_PREFIX='sudo'
+    fi
+    $SUDO_PREFIX apt install -y build-essential cmake pkg-config libssl-dev openssl git clang libclang-dev
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	echo "Found macbook"
-	brew install cmake pkg-config openssl git llvm
+    echo "Found macbook"
+    brew install cmake pkg-config openssl git llvm
 fi
 
 if [[ $(cargo --version) ]]; then
-	echo "Found cargo"
+    echo "Found cargo"
 else
-	curl https://sh.rustup.rs -sSf | sh -s -- -y
-	source $HOME/.cargo/env  
-	rustup update nightly
-	rustup target add wasm32-unknown-unknown --toolchain nightly
-	rustup update stable
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    source $HOME/.cargo/env  
 fi
-	
+
+rustup update nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup update stable
+    
 if [[ $(wasm-gc) ]]; then
-	echo "Found wasm-gc"
+    echo "Found wasm-gc"
 else
-	cargo install --git https://github.com/alexcrichton/wasm-gc
+    cargo install --git https://github.com/alexcrichton/wasm-gc
 fi
 
 RELEASE=""
