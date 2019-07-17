57dcb0d880a8a5cc491c63f52a9c1d24f2842ce5
Luke P <support@deepinthought.io>
2019-07-16 20:30:19 -0500

    Changes requested per PR conversation.  - Removed Substrate branding from heredoc. - Made the script project agnostic as requested. - Added a terminal and color checker to prevent unreadable messaging. - Tweaked the OS detection message a bit.

--- scripts/purge-chain.sh
+++ scripts/purge-chain.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 cat <<EOF
 For more options and help: ./purge-chain.sh --help
-Description: Purges local Substrate chains for DeepBrain Chain.
-Authors: "DeepBrain Chain Community <community@dbc.team>", "Luke Penrod <luke@dbc.team>"
-GitHub Organization: https://github.com/dbc-community
+Description: Purges the local database of installed chains.
 
 EOF
 
 set -e
 
-SCRIPT_VERSION="0.1.0"
+SCRIPT_VERSION="0.1.1"
 
-# * Default messages catagories *
-COLOR_DEFAULT="\033[0m"
-COLOR_ERROR="\033[91m"
-COLOR_INFO="\033[96m"
-COLOR_SUCCESS="\033[92m"
-COLOR_WARNING="\033[93m"
+# * Test color support * #
+totalColors="$(tput colors)"
+if test -t 1 && test -n "$totalColors" && test $totalColors -ge 8; then
+	# * Default messages catagories *
+	COLOR_DEFAULT="\033[0m"
+	COLOR_ERROR="\033[91m"
+	COLOR_INFO="\033[96m"
+	COLOR_SUCCESS="\033[92m"
+	COLOR_WARNING="\033[93m"
+else
+	COLOR_DEFAULT="\033[0m"
+	COLOR_ERROR="$COLOR_DEFAULT"
+	COLOR_INFO="$COLOR_DEFAULT"
+	COLOR_SUCCESS="$COLOR_DEFAULT"
+	COLOR_WARNING="$COLOR_DEFAULT"
+fi
 
 # * Configure directories *
 PROJECT_SCRIPTS="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
@@ -37,7 +45,7 @@ prompt_purge() {
 			rm -rf $dirFullPath
 			break
 			;;
-		No) break;;
+		No) break ;;
 		esac
 	done
 }
@@ -51,26 +59,26 @@ find_chain_name() {
 }
 
 print_help() {
-	echo -e "Cleans local chain specs for DeepBrain Chain.\n" \
+	echo -e "Purges the local database of installed chains.\n" \
 		"Usage ./purge-chain.sh <option>\n" \
 		"\`$0 --verbose|-v\` \t\tset the bash script to verbose mode.  Great for troubleshooting.\n" \
-		"\`$0 --chain-name NAME\` \tsets the name of the chain; i.e. dbc-mainchain\n" \
+		"\`$0 --chain-name NAME\` \tsets the name of the chain; Ommiting this option will autoscan for any chains.\n" \
 		"\`$0 -V\` \t\t\tdisplays the script version.\n"
 }
 
 purge_chain() {
 	if [[ "$OSTYPE" == "linux-gnu" ]]; then
 		OS_CHAIN_DIR="$HOME/.local/share/"
-		echo -e "$OSTYPE detected!  Setting OS_CHAIN_DIR=$OS_CHAIN_DIR"
+		echo -e "$OSTYPE detected!  Setting the OS chain directory to $OS_CHAIN_DIR"
 	elif [[ "$OSTYPE" == "darwin"* ]]; then
-		echo -e "$OSTYPE detected!"
+		echo -e "$OSTYPE detected!  Setting the OS chain directory to $OS_CHAIN_DIR"
 		OS_CHAIN_DIR="$HOME/Library/Application Support/"
 	fi
 	if [[ -z $CHAIN_NAME ]] || [[ ! -d $OS_CHAIN_DIR$CHAIN_NAME ]]; then
 		echo -e "${COLOR_ERROR}[ERROR]: ${COLOR_WARNING}Could not find an executable for the chain!${COLOR_DEFAULT}" \
 			"\n\t ${COLOR_INFO}Missing executable path:${COLOR_DEFAULT} $PWD/target/*" \
 			"\n\n\t ${COLOR_INFO}Possible remedies:${COLOR_DEFAULT}\n\t ------------------" \
-			"\n\t ${COLOR_INFO}Option 1)${COLOR_DEFAULT} cd ${PROJECT_ROOT};./scripts/init.sh;./scripts/build.sh;cargo build --release" \
+			"\n\t ${COLOR_INFO}Option 1)${COLOR_DEFAULT} cd ${PROJECT_ROOT};./scripts/init.sh;cargo build --release" \
 			"\n\t ${COLOR_INFO}Option 2)${COLOR_DEFAULT} Run ./purge-chain.sh --chain-name CHAINNAME\n"
 	fi
 	if [[ -d "$OS_CHAIN_DIR$CHAIN_NAME/chains" ]]; then
