b963e703814d967bf2ac88999efa5173edc18cb0
Drew Stone <drewstone329@gmail.com>
2019-08-09 21:04:29 +0300

    Remove rustc private

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -15,8 +15,6 @@
 // along with Edgeware.  If not, see <http://www.gnu.org/licenses/>
 
 #![warn(unused_extern_crates)]
-#![feature(rustc_private)]
-
 use std::sync::Arc;
 use std::time::Duration;
 
