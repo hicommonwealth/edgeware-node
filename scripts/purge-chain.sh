334b26b19a898e1bfa482847c2c133898114c4ce
Drew Stone <drewstone329@gmail.com>
2019-08-29 18:51:26 +0300

    Don't delete keystores

--- scripts/purge-chain.sh
+++ scripts/purge-chain.sh
@@ -4,59 +4,59 @@ db=$1
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   echo "Clearing local data from home dir: $HOME/.local/share/edgeware"
 	if [[ "$db" == "staging" ]]; then
-		rm -rf ~/.local/share/edgeware/chains/staging_testnet/
+		rm -rf ~/.local/share/edgeware/chains/staging_testnet/db/
 	elif [[ "$db" == "dev" ]]; then
-		rm -rf ~/.local/share/edgeware/chains/dev/
-		rm -rf ~/.local/share/edgeware/chains/development/
+		rm -rf ~/.local/share/edgeware/chains/dev/db/
+		rm -rf ~/.local/share/edgeware/chains/development/db/
 	elif [[ "$db" == "edgeware" ]]; then
-    	rm -rf ~/.local/share/edgeware/chains/edgeware/
-    	rm -rf ~/.local/share/edgeware/chains/edgeware_testnet/
+		rm -rf ~/.local/share/edgeware/chains/edgeware/db/
+		rm -rf ~/.local/share/edgeware/chains/edgeware_testnet/db/
 	else
 		db="all"
-	    rm -rf ~/.local/share/edgeware/chains/dev/
-	    rm -rf ~/.local/share/edgeware/chains/development/
-	    rm -rf ~/.local/share/edgeware/chains/edgeware/
-	    rm -rf ~/.local/share/edgeware/chains/edgeware_testnet/
-	    rm -rf ~/.local/share/edgeware/chains/staging_testnet/
-    	rm -rf ~/.local/share/edgeware/chains/local_testnet/
+		rm -rf ~/.local/share/edgeware/chains/dev/db/
+		rm -rf ~/.local/share/edgeware/chains/development/db/
+		rm -rf ~/.local/share/edgeware/chains/edgeware/db/
+		rm -rf ~/.local/share/edgeware/chains/edgeware_testnet/db/
+		rm -rf ~/.local/share/edgeware/chains/staging_testnet/db/
+		rm -rf ~/.local/share/edgeware/chains/local_testnet/db/
 	fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Clearing local data from home dir: $HOME/Library/Application Support/edgeware"
 	if [[ "$db" == "staging" ]]; then
-		rm -rf ~/Library/Application\ Support/edgeware/chains/staging_testnet/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/staging_testnet/db/
 	elif [[ "$db" == "dev" ]]; then
-		rm -rf ~/Library/Application\ Support/edgeware/chains/dev/
-		rm -rf ~/Library/Application\ Support/edgeware/chains/development/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/dev/db/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/development/db/
 	elif [[ "$db" == "edgeware" ]]; then
-		rm -rf ~/Library/Application\ Support/edgeware/chains/edgeware/
-		rm -rf ~/Library/Application\ Support/edgeware/chains/edgeware_testnet/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/edgeware/db/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/edgeware_testnet/db/
 	else
 		db="all"
-		rm -rf ~/Library/Application\ Support/edgeware/chains/dev/
-		rm -rf ~/Library/Application\ Support/edgeware/chains/development/
-	    rm -rf ~/Library/Application\ Support/edgeware/chains/edgeware/
-	    rm -rf ~/Library/Application\ Support/edgeware/chains/edgeware_testnet/
-	    rm -rf ~/Library/Application\ Support/edgeware/chains/staging_testnet/
-		rm -rf ~/Library/Application\ Support/edgeware/chains/local_testnet/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/dev/db/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/development/db/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/edgeware/db/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/edgeware_testnet/db/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/staging_testnet/db/
+		rm -rf ~/Library/Application\ Support/edgeware/chains/local_testnet/db/
 	fi
 else
   echo "Clearing local data from home dir: $HOME/.local/share/edgeware"
 	if [[ "$db" == "staging" ]]; then
-		rm -rf ~/.local/share/edgeware/chains/staging_testnet/
+		rm -rf ~/.local/share/edgeware/chains/staging_testnet/db/
 	elif [[ "$db" == "dev" ]]; then
-		rm -rf ~/.local/share/edgeware/chains/dev/
-		rm -rf ~/.local/share/edgeware/chains/development/
+		rm -rf ~/.local/share/edgeware/chains/dev/db/
+		rm -rf ~/.local/share/edgeware/chains/development/db/
 	elif [[ "$db" == "edgeware" ]]; then
-    	rm -rf ~/.local/share/edgeware/chains/edgeware/
-    	rm -rf ~/.local/share/edgeware/chains/edgeware_testnet/
+		rm -rf ~/.local/share/edgeware/chains/edgeware/db/
+		rm -rf ~/.local/share/edgeware/chains/edgeware_testnet/db/
 	else
 		db="all"
-	    rm -rf ~/.local/share/edgeware/chains/dev/
-	    rm -rf ~/.local/share/edgeware/chains/development/
-	    rm -rf ~/.local/share/edgeware/chains/edgeware/
-	    rm -rf ~/.local/share/edgeware/chains/edgeware_testnet/
-	    rm -rf ~/.local/share/edgeware/chains/staging_testnet/
-    	rm -rf ~/.local/share/edgeware/chains/local_testnet/
+		rm -rf ~/.local/share/edgeware/chains/dev/db/
+		rm -rf ~/.local/share/edgeware/chains/development/db/
+		rm -rf ~/.local/share/edgeware/chains/edgeware/db/
+		rm -rf ~/.local/share/edgeware/chains/edgeware_testnet/db/
+		rm -rf ~/.local/share/edgeware/chains/staging_testnet/db/
+		rm -rf ~/.local/share/edgeware/chains/local_testnet/db/
 	fi
 fi
 
