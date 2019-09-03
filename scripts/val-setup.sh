111d508775ae24e5c574080e39fee62c767a7cf5
Drew Stone <drewstone329@gmail.com>
2019-09-04 00:58:30 +0300

    Fix

--- scripts/val-setup.sh
+++ scripts/val-setup.sh
@@ -1,3 +1,3 @@
-ts-node src/index.ts -r testnet2.edgewa.re:9944 -s "${mnemonic}//stash" staking bond $controller_b58address $bond $reward_dest
-ts-node src/index.ts -r testnet2.edgewa.re:9944 -s "${mnemonic}//controller" session setKeys $pubkey1,$pubkey2,$pubkey3
-ts-node src/index.ts -s "${mnemonic}"//controller staking validate 3 0
+$path_to_cli/edge -r testnet3.edgewa.re:9944 -s "${mnemonic}//stash" staking bond $controller_b58address $bond $reward_dest
+$path_to_cli/edge -r testnet3.edgewa.re:9944 -s "${mnemonic}//controller" session setKeys $pubkey1,$pubkey2,$pubkey3
+$path_to_cli/edge -s "${mnemonic}"//controller staking validate 3 0
