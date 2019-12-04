6e8169950f228dcb1c1ef33d599ada75a4bd9385
Drew Stone <drewstone329@gmail.com>
2019-12-04 14:39:05 +0200

    Up spec for upgrade test

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -78,7 +78,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 25,
+	spec_version: 26,
 	impl_version: 26,
 	apis: RUNTIME_API_VERSIONS,
 };
