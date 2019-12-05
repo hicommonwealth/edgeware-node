8d1fd47680d4eacf0e77960a4a5e62e39ecbaa6d
Drew Stone <drewstone329@gmail.com>
2019-12-05 19:18:30 +0200

    Version bump from upgrade

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -78,8 +78,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 26,
-	impl_version: 26,
+	spec_version: 27,
+	impl_version: 27,
 	apis: RUNTIME_API_VERSIONS,
 };
 
