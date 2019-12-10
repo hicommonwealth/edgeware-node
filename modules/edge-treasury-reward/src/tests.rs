144ea7490849db60021e37f759e96f6801c56d68
dillchen <dillon@commonwealth.im>
2019-12-09 21:26:23 -0500

    fix inflation

--- modules/edge-treasury-reward/src/tests.rs
+++ modules/edge-treasury-reward/src/tests.rs
@@ -175,8 +175,8 @@ impl timestamp::Trait for Test {
 staking_reward_curve::build! {
 	const I_NPOS: PiecewiseLinear<'static> = curve!(
 		min_inflation: 0_025_000,
-		max_inflation: 0_100_000,
-		ideal_stake: 0_500_000,
+		max_inflation: 0_125_000,
+		ideal_stake: 0_800_000,
 		falloff: 0_050_000,
 		max_piece_count: 40,
 		test_precision: 0_005_000,
