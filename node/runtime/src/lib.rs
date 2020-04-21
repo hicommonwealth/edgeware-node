2bd771ce89aad8398da4456343d5298ae7f311d1
Drew Stone <drewstone329@gmail.com>
2020-04-21 16:18:08 -0600

    Up version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -80,8 +80,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 34,
-	impl_version: 34,
+	spec_version: 35,
+	impl_version: 35,
 	apis: RUNTIME_API_VERSIONS,
 };
 
