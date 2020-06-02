f0f559da4afdb7f49f7fba076f3d32b0e9759bb8
Drew Stone <drewstone329@gmail.com>
2020-06-02 09:51:34 -0600

    Fix indentation

--- node/runtime/src/impls.rs
+++ node/runtime/src/impls.rs
@@ -24,9 +24,9 @@ use sp_runtime::{Fixed128, FixedPointNumber, Perquintill};
 
 pub struct Author;
 impl OnUnbalanced<NegativeImbalance> for Author {
-    fn on_nonzero_unbalanced(amount: NegativeImbalance) {
-        Balances::resolve_creating(&Authorship::author(), amount);
-    }
+	fn on_nonzero_unbalanced(amount: NegativeImbalance) {
+		Balances::resolve_creating(&Authorship::author(), amount);
+	}
 }
 
 /// Struct that handles the conversion of Balance -> `u64`. This is used for staking's election
@@ -34,21 +34,21 @@ impl OnUnbalanced<NegativeImbalance> for Author {
 pub struct CurrencyToVoteHandler;
 
 impl CurrencyToVoteHandler {
-    fn factor() -> Balance {
-        (Balances::total_issuance() / u64::max_value() as Balance).max(1)
-    }
+	fn factor() -> Balance {
+		(Balances::total_issuance() / u64::max_value() as Balance).max(1)
+	}
 }
 
 impl Convert<Balance, u64> for CurrencyToVoteHandler {
-    fn convert(x: Balance) -> u64 {
-        (x / Self::factor()) as u64
-    }
+	fn convert(x: Balance) -> u64 {
+		(x / Self::factor()) as u64
+	}
 }
 
 impl Convert<u128, Balance> for CurrencyToVoteHandler {
-    fn convert(x: u128) -> Balance {
-        x * Self::factor()
-    }
+	fn convert(x: u128) -> Balance {
+		x * Self::factor()
+	}
 }
 
 /// Update the given multiplier based on the following formula
@@ -62,43 +62,43 @@ impl Convert<u128, Balance> for CurrencyToVoteHandler {
 pub struct TargetedFeeAdjustment<T>(sp_std::marker::PhantomData<T>);
 
 impl<T: Get<Perquintill>> Convert<Fixed128, Fixed128> for TargetedFeeAdjustment<T> {
-    fn convert(multiplier: Fixed128) -> Fixed128 {
-        let max_weight = MaximumBlockWeight::get();
-        let block_weight = System::block_weight().total().min(max_weight);
-        let target_weight = (T::get() * max_weight) as u128;
-        let block_weight = block_weight as u128;
+	fn convert(multiplier: Fixed128) -> Fixed128 {
+		let max_weight = MaximumBlockWeight::get();
+		let block_weight = System::block_weight().total().min(max_weight);
+		let target_weight = (T::get() * max_weight) as u128;
+		let block_weight = block_weight as u128;
 
-        // determines if the first_term is positive
-        let positive = block_weight >= target_weight;
-        let diff_abs = block_weight.max(target_weight) - block_weight.min(target_weight);
-        // safe, diff_abs cannot exceed u64.
-        let diff = Fixed128::saturating_from_rational(diff_abs, max_weight.max(1));
-        let diff_squared = diff.saturating_mul(diff);
+		// determines if the first_term is positive
+		let positive = block_weight >= target_weight;
+		let diff_abs = block_weight.max(target_weight) - block_weight.min(target_weight);
+		// safe, diff_abs cannot exceed u64.
+		let diff = Fixed128::saturating_from_rational(diff_abs, max_weight.max(1));
+		let diff_squared = diff.saturating_mul(diff);
 
-        // 0.00004 = 4/100_000 = 40_000/10^9
-        let v = Fixed128::saturating_from_rational(4, 100_000);
-        // 0.00004^2 = 16/10^10 Taking the future /2 into account... 8/10^10
-        let v_squared_2 = Fixed128::saturating_from_rational(8, 10_000_000_000u64);
+		// 0.00004 = 4/100_000 = 40_000/10^9
+		let v = Fixed128::saturating_from_rational(4, 100_000);
+		// 0.00004^2 = 16/10^10 Taking the future /2 into account... 8/10^10
+		let v_squared_2 = Fixed128::saturating_from_rational(8, 10_000_000_000u64);
 
-        let first_term = v.saturating_mul(diff);
-        let second_term = v_squared_2.saturating_mul(diff_squared);
+		let first_term = v.saturating_mul(diff);
+		let second_term = v_squared_2.saturating_mul(diff_squared);
 
-        if positive {
-            // Note: this is merely bounded by how big the multiplier and the inner value can go,
-            // not by any economical reasoning.
-            let excess = first_term.saturating_add(second_term);
-            multiplier.saturating_add(excess)
-        } else {
-            // Defensive-only: first_term > second_term. Safe subtraction.
-            let negative = first_term.saturating_sub(second_term);
-            multiplier
-                .saturating_sub(negative)
-                // despite the fact that apply_to saturates weight (final fee cannot go below 0)
-                // it is crucially important to stop here and don't further reduce the weight fee
-                // multiplier. While at -1, it means that the network is so un-congested that all
-                // transactions have no weight fee. We stop here and only increase if the network
-                // became more busy.
-                .max(Fixed128::saturating_from_integer(-1))
-        }
-    }
+		if positive {
+			// Note: this is merely bounded by how big the multiplier and the inner value can go,
+			// not by any economical reasoning.
+			let excess = first_term.saturating_add(second_term);
+			multiplier.saturating_add(excess)
+		} else {
+			// Defensive-only: first_term > second_term. Safe subtraction.
+			let negative = first_term.saturating_sub(second_term);
+			multiplier
+				.saturating_sub(negative)
+				// despite the fact that apply_to saturates weight (final fee cannot go below 0)
+				// it is crucially important to stop here and don't further reduce the weight fee
+				// multiplier. While at -1, it means that the network is so un-congested that all
+				// transactions have no weight fee. We stop here and only increase if the network
+				// became more busy.
+				.max(Fixed128::saturating_from_integer(-1))
+		}
+	}
 }
