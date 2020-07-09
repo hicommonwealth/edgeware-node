b3d6e9e3d531969dfbb1e30265ee356884a92b75
Alexander Popiak <alexander.popiak@parity.io>
2020-07-09 11:27:59 +0200

    bump spec_version and remove panic

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -103,8 +103,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 36,
-	impl_version: 36,
+	spec_version: 37,
+	impl_version: 37,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
 };
@@ -913,8 +913,6 @@ impl_runtime_apis! {
 			source: TransactionSource,
 			tx: <Block as BlockT>::Extrinsic,
 		) -> TransactionValidity {
-			sp_runtime::print("runtime/lib/validate_transaction");
-			panic!("runtime/lib/validate_transaction");
 			Executive::validate_transaction(source, tx)
 		}
 	}
