edbebcc87cf41b85a0f9de48f252f7cd7fba8818
Drew Stone <drewstone329@gmail.com>
2020-09-15 00:14:02 -0400

    Add precompiles at addresses

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -87,7 +87,7 @@ pub use pallet_balances::Call as BalancesCall;
 pub use frame_system::Call as SystemCall;
 #[cfg(any(feature = "std", test))]
 pub use pallet_staking::StakerStatus;
-
+/// Public precompiles from evm module
 pub use pallet_evm::precompiles::{Sha256, Ripemd160, ECRecover};
 /// Implementations of some helper traits passed into runtime modules as associated types.
 pub mod impls;
@@ -857,12 +857,93 @@ impl pallet_evm::Trait for Runtime {
 	type AddressMapping = HashedAddressMapping<BlakeTwo256>;
 	type Currency = Balances;
 	type Event = Event;
-	type Precompiles = (Sha256, Ripemd160, ECRecover);
+	type Precompiles = EdgewarePrecompiles;
 	type ChainId = ChainId;
 
 	/// EVM config used in the module.
 	fn config() -> &'static Config {
 		&EVM_CONFIG
+	}
+}
+
+pub struct EdgewarePrecompiles;
+
+type PrecompiledCallable = fn(&[u8], Option<usize>)
+	-> core::result::Result<(pallet_evm::ExitSucceed, Vec<u8>, usize), pallet_evm::ExitError>;
+
+impl pallet_evm::Precompiles for EdgewarePrecompiles {
+	fn execute(
+		address: H160,
+		input: &[u8],
+		target_gas: Option<usize>
+	) -> Option<core::result::Result<(pallet_evm::ExitSucceed, Vec<u8>, usize), pallet_evm::ExitError>> {
+		match get_precompiled_func_from_address(&address) {
+		   Some(func) => return Some(func(input, target_gas)),
+		   _ => {},
+		};
+
+		None
+	}
+}
+
+fn get_precompiled_func_from_address(address: &H160) -> Option<PrecompiledCallable> {
+	use core::str::FromStr;
+	use pallet_evm::Precompile;
+
+	// identity
+	let addr_ecrecover = H160::from_str("0000000000000000000000000000000000000001").expect("Invalid address at precompiles generation");
+	// ecrecover
+	let addr_sha256 = H160::from_str("0000000000000000000000000000000000000002").expect("Invalid address at precompiles generation");
+	// sha256
+	let addr_ripemd160 = H160::from_str("0000000000000000000000000000000000000003").expect("Invalid address at precompiles generation");
+	// ripemd160
+	let addr_modexp = H160::from_str("0000000000000000000000000000000000000004").expect("Invalid address at precompiles generation");
+	// modexp
+	let addr_alt_bn128_add = H160::from_str("0000000000000000000000000000000000000005").expect("Invalid address at precompiles generation");
+	// alt_bn128_add
+	let addr_alt_bn128_mul = H160::from_str("0000000000000000000000000000000000000006").expect("Invalid address at precompiles generation");
+	// alt_bn128_mul
+	let addr_alt_bn128_pairing = H160::from_str("0000000000000000000000000000000000000007").expect("Invalid address at precompiles generation");
+	// alt_bn128_pairing
+	let addr_blake2_f = H160::from_str("0000000000000000000000000000000000000008").expect("Invalid address at precompiles generation");
+	// blake2_f
+	let addr_bls12_381_g1_add = H160::from_str("0000000000000000000000000000000000000009").expect("Invalid address at precompiles generation");
+	// bls12_381_g1_add
+	let addr_bls12_381_g1_mul = H160::from_str("000000000000000000000000000000000000000A").expect("Invalid address at precompiles generation");
+	// bls12_381_g1_mul
+	let addr_bls12_381_g1_multiexp = H160::from_str("0000000000000000000000000000000000000000B").expect("Invalid address at precompiles generation");
+	// bls12_381_g1_multiexp
+	let addr_bls12_381_g2_add = H160::from_str("000000000000000000000000000000000000000C").expect("Invalid address at precompiles generation");
+	// bls12_381_g2_add
+	let addr_bls12_381_g2_mul = H160::from_str("0000000000000000000000000000000000000000D").expect("Invalid address at precompiles generation");
+	// bls12_381_g2_mul
+	let addr_bls12_381_g2_multiexp = H160::from_str("0000000000000000000000000000000000000000E").expect("Invalid address at precompiles generation");
+	// bls12_381_g2_multiexp
+	let addr_bls12_381_pairing = H160::from_str("000000000000000000000000000000000000000F").expect("Invalid address at precompiles generation");
+	// bls12_381_pairing
+	let addr_bls12_381_fp_to_g1 = H160::from_str("0000000000000000000000000000000000000010").expect("Invalid address at precompiles generation");
+	// bls12_381_fp_to_g1
+	let addr_bls12_381_fp2_to_g2 = H160::from_str("0000000000000000000000000000000000000011").expect("Invalid address at precompiles generation");
+
+	match *address {
+		addr_ecrecover => Some(ECRecover::execute),
+		addr_sha256 => Some(Sha256::execute),
+		addr_ripemd160 => Some(Ripemd160::execute),
+		// addr_modexp => Some(Modexp::execute),
+		// addr_alt_bn128_add => Some(Bn128Add::execute),
+		// addr_alt_bn128_mul => Some(Bn128Mul::execute),
+		// addr_alt_bn128_pairing => Some(Bn128Pairing::execute),
+		// addr_blake2_f => Some(Blake2F::execute),
+		// addr_bls12_381_g1_add => Some(Bls12G1Add::execute),
+		// addr_bls12_381_g1_mul => Some(Bls12G1Mul::execute),
+		// addr_bls12_381_g1_multiexp => Some(Bls12G1MultiExp::execute),
+		// addr_bls12_381_g2_add => Some(Bls12G2Add::execute),
+		// addr_bls12_381_g2_mul => Some(Bls12G2Mul::execute),
+		// addr_bls12_381_g2_multiexp => Some(Bls12G2MultiExp::execute),
+		// addr_bls12_381_pairing => Some(Bls12Pairing::execute),
+		// addr_bls12_381_fp_to_g1 => Some(Bls12MapFpToG1::execute),
+		// addr_bls12_381_fp2_to_g2 => Some(Bls12MapFp2ToG2::execute),
+		_ => None
 	}
 }
 
