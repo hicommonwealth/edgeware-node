f954818bb79e1bc9a522cb463ca6a7dcbb016760
Jake Naviasky <jake@commonwealth.im>
2021-01-04 18:34:38 -0500

    Fix ECRecovery test.

--- ts-tests/frontier-tester/tests/testECRecovery.js
+++ ts-tests/frontier-tester/tests/testECRecovery.js
@@ -28,9 +28,9 @@ describeWithEdgeware('ECRecovery test', async (context) => {
 
   it('should interact with precompile directly', async () => {
     const web3 = context.web3;
-    const ECRECOVER_PRECOMPILE_ADDRESS = '0000000000000000000000000000000000000001';
+    const ECRECOVER_PRECOMPILE_ADDRESS = '0000000000000000000000000000000000000000';
 
-    const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit..'
+    const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tubulum fuisse, qua illum, cuius is condemnatus est rogatione, P. Eaedem res maneant alio modo.'
     const messageHex = '0x' + Buffer.from(message).toString('hex');
     const sig = (await web3.eth.sign(messageHex, account)).slice(2);
     const r = `${sig.slice(0, 64)}`
@@ -41,8 +41,7 @@ describeWithEdgeware('ECRecovery test', async (context) => {
 
     const RAW_TX = {
       from: account,
-      gasPrice: "0x01",
-      gas: web3.utils.toWei('1', 'ether'),
+      gas: '27720',
       to: ECRECOVER_PRECOMPILE_ADDRESS,
       value: "0x0",
       data: `0x${hash.toString('hex')}${sigPart}`,
@@ -53,17 +52,11 @@ describeWithEdgeware('ECRecovery test', async (context) => {
       privKey
     );
 
-    const estimatedGas = await web3.eth.estimateGas({
-      from: account,
-      to: ECRECOVER_PRECOMPILE_ADDRESS,
-      value: '0x0',
-      data: `0x${hash.toString('hex')}${sigPart}`,
-    });
     const tx = await web3.eth.sendTransaction({
       from: account,
       to: ECRECOVER_PRECOMPILE_ADDRESS,
       value: '0x0',
-      gas: estimatedGas,
+      gas: '27720',
       data: `0x${hash.toString('hex')}${sigPart}`,
     });
 
