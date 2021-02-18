dec990a8e251c0814266cd058546ff992062291b
Jake Naviasky <jake@commonwealth.im>
2021-02-18 15:51:45 -0500

    Add docker-compose scripts. (#287)
    * Add docker-compose scripts.
    
    * Switch dev docker-compose to use v3.2.0 explicitly.
    
    * Update readme and add local build dockerfile.

--- build_and_run_devnet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp .dockerignore .dockerignore.original
-ln -fs .dockerignore.remotecargo .dockerignore
-docker build -t cwl/edgeware -f remotecargo.dockerfile .
-docker run -it cwl/edgeware --dev
