119ac21385c6fdb9599e2a721b1e49592173ae3c
Drew Stone <drewstone329@gmail.com>
2021-05-08 21:53:07 +0300

    Ups to evm 0.26

---
+++ node/runtime/src/precompiles.rs
@@ -0,0 +1,73 @@
+use codec::Decode;
+use evm::{Context, ExitError, ExitSucceed};
+use frame_support::dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo};
+use pallet_evm::{Precompile, PrecompileSet};
+use pallet_evm_precompile_blake2::Blake2F;
+use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
+use pallet_evm_precompile_dispatch::Dispatch;
+use pallet_evm_precompile_modexp::Modexp;
+use pallet_evm_precompile_ed25519::Ed25519Verify;
+use pallet_evm_precompile_sha3fips::{Sha3FIPS256, Sha3FIPS512};
+use pallet_evm_precompile_simple::{ECRecover, Identity, Ripemd160, Sha256, ECRecoverPublicKey};
+use sp_core::H160;
+
+use sp_std::fmt::Debug;
+use sp_std::{marker::PhantomData, vec::Vec};
+
+
+
+#[derive(Debug, Clone, Copy)]
+pub struct EdgewarePrecompiles<R>(PhantomData<R>);
+
+impl<R: frame_system::Config> EdgewarePrecompiles<R>
+{
+	/// Return all addresses that contain precompiles. This can be used to populate dummy code
+	/// under the precompile, and potentially in the future to prevent using accounts that have
+	/// precompiles at their addresses explicitly using something like SignedExtra.
+	#[allow(dead_code)]
+	fn used_addresses() -> impl Iterator<Item = H160> {
+		sp_std::vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 1024, 1025, 1026, 1027, 1028]
+			.into_iter()
+			.map(|x| hash(x).into())
+	}
+}
+
+/// The following distribution has been decided for the precompiles
+/// 0-1023: Ethereum Mainnet Precompiles
+/// 1024-2047 Precompiles that are not in Ethereum Mainnet but are neither Moonbeam specific
+impl<R: frame_system::Config + pallet_evm::Config> PrecompileSet for EdgewarePrecompiles<R>
+where
+	R::Call: Dispatchable<PostInfo = PostDispatchInfo> + GetDispatchInfo + Decode,
+	<R::Call as Dispatchable>::Origin: From<Option<R::AccountId>>,
+{
+	fn execute(
+		address: H160,
+		input: &[u8],
+		target_gas: Option<u64>,
+		context: &Context,
+	) -> Option<Result<(ExitSucceed, Vec<u8>, u64), ExitError>> {
+		match address {
+			// Ethereum precompiles
+			a if a == hash(1) => Some(ECRecover::execute(input, target_gas, context)),
+			a if a == hash(2) => Some(Sha256::execute(input, target_gas, context)),
+			a if a == hash(3) => Some(Ripemd160::execute(input, target_gas, context)),
+			a if a == hash(4) => Some(Identity::execute(input, target_gas, context)),
+			a if a == hash(5) => Some(Modexp::execute(input, target_gas, context)),
+			a if a == hash(6) => Some(Bn128Add::execute(input, target_gas, context)),
+			a if a == hash(7) => Some(Bn128Mul::execute(input, target_gas, context)),
+			a if a == hash(8) => Some(Bn128Pairing::execute(input, target_gas, context)),
+			a if a == hash(9) => Some(Blake2F::execute(input, target_gas, context)),
+			// Non-Edgeware specific nor Ethereum precompiles :
+			a if a == hash(1024) => Some(Dispatch::<R>::execute(input, target_gas, context)),
+			a if a == hash(1025) => Some(Sha3FIPS256::execute(input, target_gas, context)),
+			a if a == hash(1026) => Some(Sha3FIPS512::execute(input, target_gas, context)),
+			a if a == hash(1027) => Some(ECRecoverPublicKey::execute(input, target_gas, context)),
+			a if a == hash(1028) => Some(Ed25519Verify::execute(input, target_gas, context)),
+			_ => None,
+		}
+	}
+}
+
+fn hash(a: u64) -> H160 {
+	H160::from_low_u64_be(a)
+}
