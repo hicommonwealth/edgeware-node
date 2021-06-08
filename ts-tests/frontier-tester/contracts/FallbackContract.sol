c244b20089db29785390e749918d09b12a3b8edf
drewstone <drewstone329@gmail.com>
2021-06-08 09:13:47 -0400

    Remove frontier tester (#306)
    * remove frontier tester, moved out of repo
    
    * Fix ci script

--- ts-tests/frontier-tester/contracts/FallbackContract.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-contract FallbackContract {
-  constructor() public { }
-
-	function () external payable {
-		msg.sender.transfer(msg.value);
-	}
-}
--- ts-tests/frontier-tester/contracts/IContract.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.5.0;
-
-interface IContract {
-	function doSomething() external view returns (bool);
-}
